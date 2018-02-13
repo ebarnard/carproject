39ab5339d8f31c1487e978374159b635428aa43e
Niki Limonov <nikolay.a.limonov@gmail.com>
2018-02-13 16:05:48 +0000

    Calibration working with test board.

--- vision_python/aruco.py
+++ vision_python/aruco.py
@@ -26,20 +26,75 @@ cap = cv2.VideoCapture("../video/aruco_sheet_4x4_50_small.avi")
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    gray = cv2.imread("../video/aruco_4x4_50_test_board.png", cv2.IMREAD_GRAYSCALE)
     # print(frame.shape) #480x640
     # Our operations on the frame come here
     # frame = frame[400:880, 300:940] #480x640
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_thresh = gray.copy()
     cv2.adaptiveThreshold(gray, maxValue=255, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=5, C=0,
                           dst=gray_thresh)
     # MDetector.setDictionary("ARUCO_MIP_36h12")
     aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
+    markerBottomLeftCorners = [[0, 0], [0.6, 0], [0, 0.3], [0.6, 0.6], [0.6, 0.9], [-0.3, 0.6]]
+    markerWidth = 0.035
+    objPoints = []
+    for i in range(0, len(markerBottomLeftCorners)):
+        [x, y] = markerBottomLeftCorners[i]
+        corners = np.array([
+            x, y, 0,
+            x, y + markerWidth, 0,
+            x + markerWidth, y + markerWidth, 0,
+            x + markerWidth, y, 0,
+        ])
+        objPoints.append(corners)
+
+    objPoints = np.array(objPoints ,('float32'))
+    ids = np.array([27, 18, 5, 12, 43, 42])
+    board = aruco.Board_create(objPoints, aruco_dict, ids)
+
     parameters = aruco.DetectorParameters_create()
     # lists of ids and the corners belonging to each id
-    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray_thresh, aruco_dict, parameters=parameters)
+    corners, ids, rejectedCorners = aruco.detectMarkers(gray_thresh, aruco_dict, parameters=parameters)
     print("corners: ", corners)
+
+    corners, ids, rejectedCorners, _ = aruco.refineDetectedMarkers(gray_thresh, board, corners, ids, rejectedCorners)
+    print("refined corners: ", corners)
+
+    # Calibration requires a weird input format
+    cornersConcatenated = []
+    idsConcatenated = []
+    markerCounterPerFrame = []
+
+    for i in range(0, len(ids)):
+        markerCounterPerFrame.append(len(corners[i]))
+        for j in range(0, len(corners[i])):
+            cornersConcatenated.append(corners[i][j])
+            idsConcatenated.append(ids[i][j])
+
+    cornersConcatenated = np.array(cornersConcatenated)
+    idsConcatenated = np.array(idsConcatenated)
+    markerCounterPerFrame = np.array(markerCounterPerFrame)
+
+    retval, cameraMatrix, distCoeffs, _, _ = aruco.calibrateCameraAruco(cornersConcatenated, idsConcatenated, markerCounterPerFrame, board, gray.shape, None, None)
+
+    ret, rvec, tvec = aruco.estimatePoseBoard(corners, ids, board, cameraMatrix, distCoeffs)
+    R, _ = cv2.Rodrigues(rvec)
+
+    P = np.zeros((4, 4))
+    P[0:3, 0:3] = R
+    P[0:3, 3] = tvec[0:3, 0]
+    P[3, 3] = 1
+
+    one = np.array([0, 0, 0, 1])
+    v = cameraMatrix.dot(P.dot(one)[0:3])
+    print("world origin", v / v[2])
+
+    print("camera matrix", cameraMatrix)
+    print("rot", R)
+    print("tr", tvec)
 
     # It's working.
     # my problem was that the cellphone put black all around it. The algorithm
