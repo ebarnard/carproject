c5bda6e18f9b87a35a1a32e506e3e5b091d2e93b
Edward Barnard <eabarnard@gmail.com>
2018-02-18 11:07:51 +0000

    Find the car position using a scaled image before refining using the full image in the tracker. This reduces tracking time by at least a factor of five on my machine.

--- rust_controller/car_tracker/src/lib.rs
+++ rust_controller/car_tracker/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(iterator_step_by)]
+
 extern crate faster;
 extern crate prelude;
 
@@ -45,73 +47,78 @@ impl Tracker {
     pub fn track_frame(&mut self, frame: &[u8]) -> (float, float, float) {
         assert_eq!(self.bg.len(), frame.len());
 
-        let delta_norm_max = 200.0;
-
-        // Subtract background and threshold at 50. Fill fg with the coordinates of foreground
-        // pixels.
+        // Subtract background and threshold at 50.
+        //
+        // This simpler code optimises very poorly so we use explicit SIMD.
+        //for ((&a, &b), out) in frame.iter().zip(&self.bg).zip(&mut self.fg_thresh) {
+        //    *out = a <= b || a - b <= thresh
+        //    if  {
+        //        *out = 0;
+        //    } else {
+        //        *out = 1;
+        //    }
+        //}
         let thresh = 50;
-
-        // This optimises very poorly
-        /*for ((&a, &b), out) in frame.iter().zip(&self.bg).zip(&mut self.fg_thresh) {
-            *out = a <= b || a - b <= thresh
-            if  {
-                *out = 0;
-            } else {
-                *out = 1;
-            }
-        }*/
-
         (frame.simd_iter(u8s(0)), self.bg.simd_iter(u8s(0)))
             .zip()
             .simd_map(|(a, b)| a.saturating_sub(b).gt(u8s(thresh)).as_u8s())
             .scalar_fill(&mut self.fg_thresh);
 
+        // Fill `fg` with the coordinates of foreground pixels.
+        // The image is scaled to look at only one in every four pixels.
+        let scale_step = 4;
         self.fg.truncate(0);
-        for (y, row) in self.fg_thresh.chunks(self.x_bins.len()).enumerate() {
-            for (x, &v) in row.iter().enumerate() {
-                if v != 0 {
-                    self.fg.push((x as u32, y as u32));
-                }
+        for (y, row) in self.fg_thresh
+            .chunks(self.x_bins.len())
+            .enumerate()
+            .step_by(scale_step)
+        {
+            for (x, _) in row.iter()
+                .enumerate()
+                .step_by(scale_step)
+                .filter(|&(_, &v)| v != 0)
+            {
+                self.fg.push((x as u32, y as u32));
             }
         }
 
-        // Deal with no foreground pixels here (explicit return no coords)
-
-        // Find the median foreground pixel coordinates
-        self.x_bins.iter_mut().for_each(|v| *v = 0);
-        self.y_bins.iter_mut().for_each(|v| *v = 0);
-        for &(x, y) in &self.fg {
-            self.x_bins[x as usize] += 1;
-            self.y_bins[y as usize] += 1;
+        // Return early if there are no foreground pixels.
+        // TODO: Return None if there is an unexpected number of foreground pixels.
+        if self.fg.len() == 0 {
+            return (0.0, 0.0, 0.0);
         }
 
-        let mid = (self.fg.len() / 2) as u32;
-        let (x_median, _) = self.x_bins
-            .iter()
-            .scan(0, |acc, &n| {
-                *acc += n;
-                Some(*acc)
-            })
-            .enumerate()
-            .filter(|&(_, acc)| acc >= mid)
-            .next()
-            .unwrap_or((0, 0));
+        // Find the median foreground pixel coordinate of the scaled image
+        let (x_median_scaled, y_median_scaled) =
+            find_median_x_y(&self.fg, &mut self.x_bins, &mut self.y_bins);
 
-        let (y_median, _) = self.y_bins
-            .iter()
-            .scan(0, |acc, &n| {
-                *acc += n;
-                Some(*acc)
-            })
+        // Fill `fg` with the coordinates of foreground pixels.
+        // Only pixels around the scaled image median are considered.
+        let search_radius = 200;
+        let x_min = x_median_scaled.saturating_sub(search_radius / 2) as usize;
+        let y_min = y_median_scaled.saturating_sub(search_radius / 2) as usize;
+        self.fg.truncate(0);
+        for (y, row) in self.fg_thresh
+            .chunks(self.x_bins.len())
             .enumerate()
-            .filter(|&(_, acc)| acc >= mid)
-            .next()
-            .unwrap_or((0, 0));
+            .skip(y_min)
+            .take(search_radius as usize)
+        {
+            for (x, _) in row.iter()
+                .enumerate()
+                .skip(x_min)
+                .take(search_radius as usize)
+                .filter(|&(_, &v)| v != 0)
+            {
+                self.fg.push((x as u32, y as u32));
+            }
+        }
 
-        // Find the mean theta
-        let mut n_inliers = 0;
+        // Find the median foreground pixel coordinate of the restricted image.
+        let (x_median, y_median) = find_median_x_y(&self.fg, &mut self.x_bins, &mut self.y_bins);
+
+        // Find the mean angle between the median coordinate and the other coordinates.
         let mut position_cov = Matrix2::zeros();
-
         for &(x, y) in &self.fg {
             let delta_x = x as f64 - x_median as f64;
             let delta_y = y as f64 - y_median as f64;
@@ -119,15 +126,12 @@ impl Tracker {
             // This is somewhat equivalent to finding the mean angle
             // Add eps to avoid dividing zero by zero
             let delta_norm = f64::hypot(delta_x, delta_y) + ::std::f64::EPSILON;
-            if delta_norm <= delta_norm_max {
-                n_inliers += 1;
-                let val = Vector2::new(delta_x / delta_norm, delta_y / delta_norm);
-                position_cov += &val * val.transpose();
-            }
+            let val = Vector2::new(delta_x / delta_norm, delta_y / delta_norm);
+            position_cov += &val * val.transpose();
         }
 
-        // Divide by the population size to make a covariance matrix
-        let position_cov = position_cov / n_inliers as f64;
+        // Divide by the population size to make it a covariance matrix.
+        let position_cov = position_cov / self.fg.len() as f64;
 
         // Find largest eigenvalue and corresponding eigenvector of the covariance matrix.
         let eigen = position_cov.symmetric_eigen();
@@ -146,4 +150,39 @@ impl Tracker {
 
         (x_median as float, y_median as float, theta)
     }
+}
+
+fn find_median_x_y(fg: &[(u32, u32)], x_bins: &mut [u32], y_bins: &mut [u32]) -> (u32, u32) {
+    x_bins.iter_mut().for_each(|v| *v = 0);
+    y_bins.iter_mut().for_each(|v| *v = 0);
+
+    for &(x, y) in fg {
+        x_bins[x as usize] += 1;
+        y_bins[y as usize] += 1;
+    }
+
+    let mid = (fg.len() / 2) as u32;
+    let (x_median, _) = x_bins
+        .iter()
+        .scan(0, |acc, &n| {
+            *acc += n;
+            Some(*acc)
+        })
+        .enumerate()
+        .filter(|&(_, acc)| acc >= mid)
+        .next()
+        .unwrap_or((0, 0));
+
+    let (y_median, _) = y_bins
+        .iter()
+        .scan(0, |acc, &n| {
+            *acc += n;
+            Some(*acc)
+        })
+        .enumerate()
+        .filter(|&(_, acc)| acc >= mid)
+        .next()
+        .unwrap_or((0, 0));
+
+    (x_median as u32, y_median as u32)
 }
