9186031817d6a75564c9dbbbdc58c6cd06b179a8
Edward Barnard <eabarnard@gmail.com>
2018-02-26 15:13:26 +0000

    Improve SimulationModel creation macros.

--- rust_controller/simulator/src/simulation_model.rs
+++ rust_controller/simulator/src/simulation_model.rs
@@ -30,21 +30,11 @@ pub fn model_from_config(config: &SimulatorConfig) -> Box<SimulationModel> {
         .1(&config.params)
 }
 
-static MODELS: &'static [(fn() -> &'static str, fn(&[float]) -> Box<SimulationModel>)] = &[
-    (DirectVelocity::name, new::<DirectVelocity>),
-    (SpenglerGammeter::name, new::<SpenglerGammeter>),
-    (NoSlipPoint::name, new::<NoSlipPoint>),
-];
-
-fn new<T: 'static + SimulationModel>(params: &[float]) -> Box<SimulationModel> {
-    Box::new(T::new(params))
-}
-
-macro_rules! simulation_control_model(
-    ($name:ident, $model:ty) => (
+macro_rules! simulation_control_model_impl {
+    ($name:ident) => {
         pub struct $name {
-            model: $model,
-            params: Vector<<$model as ControlModel>::NP>,
+            model: control_model::$name,
+            params: Vector<<control_model::$name as ControlModel>::NP>,
         }
 
         impl SimulationModel for $name {
@@ -53,17 +43,18 @@ macro_rules! simulation_control_model(
                 Self: Sized
             {
                 $name {
-                    params: Vector::<<$model as ControlModel>::NP>::from_column_slice(params),
-                    model: <$model as ControlModel>::new(),
+                    params: Vector::<<control_model::$name as ControlModel>::NP>
+                        ::from_column_slice(params),
+                    model: <control_model::$name as ControlModel>::new(),
                 }
             }
 
             fn name() -> &'static str {
-                <$model as ControlModel>::name()
+                <control_model::$name as ControlModel>::name()
             }
 
             fn init_state(&self, x: float, y: float, heading: float) -> SimState {
-                let mut state = vec![0.0; <$model as ControlModel>::NS::dim()];
+                let mut state = vec![0.0; <control_model::$name as ControlModel>::NS::dim()];
                 state[0] = x;
                 state[1] = y;
                 state[2] = heading;
@@ -71,21 +62,35 @@ macro_rules! simulation_control_model(
             }
 
             fn inspect_state(&self, state: &SimState) -> State {
-                let x = Vector::<<$model as ControlModel>::NS>::from_column_slice(&state.0);
+                let x = Vector::<<control_model::$name as ControlModel>::NS>
+                    ::from_column_slice(&state.0);
                 self.model.x_to_state(&x)
             }
 
             fn step(&mut self, dt: float, mut state: SimState, control: &Control) -> SimState {
-                let x = Vector::<<$model as ControlModel>::NS>::from_column_slice(&state.0);
+                let x = Vector::<<control_model::$name as ControlModel>::NS>
+                    ::from_column_slice(&state.0);
                 let u = self.model.u_from_control(control);
                 let x_dt = self.model.step(dt, &x, &u, &self.params);
                 state.0.copy_from_slice(x_dt.as_slice());
                 state
             }
         }
-    );
-);
+    };
+}
 
-simulation_control_model!(DirectVelocity, control_model::DirectVelocity);
-simulation_control_model!(SpenglerGammeter, control_model::SpenglerGammeter);
-simulation_control_model!(NoSlipPoint, control_model::NoSlipPoint);
+macro_rules! simulation_control_models {
+    {$($name:ident,)*} => {
+        $(simulation_control_model_impl!($name);)*
+
+        static MODELS: &'static [(fn() -> &'static str, fn(&[float]) -> Box<SimulationModel>)] = &[
+            $(($name::name, |params| Box::new($name::new(params))),)*
+        ];
+    };
+}
+
+simulation_control_models! {
+    DirectVelocity,
+    SpenglerGammeter,
+    NoSlipPoint,
+}
